bde70a583b7aca2c1d3f75bd8ec9d4c270d01b5a
AYoonesi <alireza.yoonesi78@gmail.com>
2023-03-18 04:19:07 +0330

    codon finished

--- jadi/is_prime.py
+++ jadi/is_prime.py
@@ -1,6 +1,9 @@
 '''
     For this video on codon:  https://www.youtube.com/watch?v=chkHgLH_KVU
     اسکریپت‌های پایتونی‌تون رو با کامپایل کردن، تا صد برابر سریعتر کنین
+
+    Final result: It's true, for this great count it enhanced the program 240 times. In fact reduced 6s to 24ms!
+    The exe file is attached. It was created using codon.
 '''
 def is_prime(n):
     for i in range(2, int(n/2)+1):
@@ -9,7 +12,7 @@ def is_prime(n):
         return True
 
 count = 0
-for i in range(2, 100000):
+for i in range(2, 10000000):
     if (is_prime(i)):
         count += 1
 print(count)
