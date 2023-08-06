b4d7471a0cb5681a5ca2349c2a1fa6c47026e42f
AYoonesi <alireza.yoonesi78@gmail.com>
2023-08-06 04:45:11 +0330

    real life scripts

---
+++ real-life-scripts/pdf-slpit.py
@@ -0,0 +1,29 @@
+import os
+import PyPDF2
+
+def split_pdf(input_path, output_path, start_page, end_page):
+    with open(input_path, 'rb') as file:
+        pdf = PyPDF2.PdfReader(file)
+
+        # Create a new PDF writer
+        output_pdf = PyPDF2.PdfWriter()
+
+        # Iterate through pages to extract desired range
+        for page_number in range(start_page-1, end_page):
+            page = pdf.pages[page_number]
+            output_pdf.add_page(page)
+
+        # Write the output to a new file
+        with open(output_path, 'wb') as new_file:
+            output_pdf.write(new_file)
+        
+        print(f'File saved in {output_path}.')
+
+# Example usage
+script_directory = os.path.dirname(os.path.abspath(__file__))
+input_file = os.path.join(script_directory, 'Handbook of development economics_Vol5_2010.pdf')
+output_file = os.path.join(script_directory, 'Kose, A., E. Prasad, K. Rogoff, and S.-J. Wei. 2010. Financial Globalization and Economic Policies.pdf')
+start_page = 288
+end_page = 367
+
+split_pdf(input_file, output_file, start_page, end_page)
