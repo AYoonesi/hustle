705623668d99f6f61e23a7b27f8ab7c2b84420bc
AYoonesi <alireza.yoonesi78@gmail.com>
2024-01-22 15:47:18 +0330

    The 13 Steps of Successful Academic Legal Research

--- real-life-scripts/pdf-merge.py
+++ real-life-scripts/pdf-merge.py
@@ -15,7 +15,7 @@ def merge_pdf(input_paths = [], output_path = 'merge-output.pdf'):
 # Example usage
 script_directory = os.path.dirname(os.path.abspath(__file__))
 input_paths = [
-    os.path.join(script_directory, 'jazaa1.pdf'),
-    os.path.join(script_directory, 'takmili.pdf'),
+    os.path.join(script_directory, 'The 13 Steps of Successful Academic Legal Research - p1.pdf'),
+    os.path.join(script_directory, 'The 13 Steps of Successful Academic Legal Research -et.pdf'),
 ]
-merge_pdf(input_paths, 'jaza-merged.pdf')
+merge_pdf(input_paths, 'The 13 Steps of Successful Academic Legal Research - merged.pdf')
